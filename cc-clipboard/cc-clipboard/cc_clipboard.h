ed8ffbc43c57d8f09b513933039dc32a424a86b9
hello_chenchen <welcomechenchen@gmail.com>
2018-12-31 19:06:06 +0800

    first commit, do not know how to write a libary in mac

---
+++ cc-clipboard/cc-clipboard/cc_clipboard.h
@@ -0,0 +1,13 @@
+//
+//  cc_clipboard.h
+//  cc-clipboard
+//
+//  Created by Maiken on 31/12/2018.
+//  Copyright © 2018 hello_chenchen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface cc_clipboard : NSObject
+
+@end
---
+++ cc-clipboard/cc-clipboard/cc_clipboard.m
@@ -0,0 +1,13 @@
+//
+//  cc_clipboard.m
+//  cc-clipboard
+//
+//  Created by Maiken on 31/12/2018.
+//  Copyright © 2018 hello_chenchen. All rights reserved.
+//
+
+#import "cc_clipboard.h"
+
+@implementation cc_clipboard
+
+@end
