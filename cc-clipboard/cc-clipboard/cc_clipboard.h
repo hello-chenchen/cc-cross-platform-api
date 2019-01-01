77fd0eef0ca745dcf30def68fee7e993f2aba48d
hello_chenchen <welcomechenchen@gmail.com>
2019-01-01 15:35:34 +0800

    first release library

--- cc-clipboard/cc-clipboard/cc_clipboard.h
+++ cc-clipboard/cc-clipboard/cc_clipboard.h
@@ -7,7 +7,16 @@
 //
 
 #import <Foundation/Foundation.h>
+//#import <Cocoa/Cocoa.h>
+//#import <AppKit/AppKit.h>
+#import <AppKit/NSPasteboard.h>
 
-@interface cc_clipboard : NSObject
+@interface cc_clipboard: NSObject{
+    NSPasteboardType m_pasteBoardType;
+    NSPasteboard* m_clipboard;
+//    @property (nonatomic) NSPasteboard * clipboard;
+}
+
+-(NSPasteboardType) getPasteBoardType;
 
 @end
